095ffe7baba75d86112bbb7e7f8767d313e0e5a5
YoussefBakry5 <125702757+YoussefBakry5@users.noreply.github.com>
2023-05-22 20:45:03 +0200

    ..

--- controllers/forget.js
+++ controllers/forget.js
@@ -1,29 +1,29 @@
-const Clients = require("../models/clientschema");
-var random=require("uuid");
+// const Clients = require("../models/clientschema");
+// var random=require("uuid");
 
 
-exports.generateResetPasswordEmail = async (req, res) => {
-    console.log("----------------");
-    const email2  = req.body.email;
-    console.log("======================");
-    console.log("======================");
-    console.log(email2);
+// exports.generateResetPasswordEmail = async (req, res) => {
+//     console.log("----------------");
+//     const email2  = req.body.email;
+//     console.log("======================");
+//     console.log("======================");
+//     console.log(email2);
     
-    var token = "test";
+//     var token = "test";
 
-    const clients = await Clients.find({Email: email2});
-    if (clients == undefined || clients ==  null || clients.length== 0)
-     {
-        console.log("Could not find usre");
-        return res.send("Could not find user.");
-     }
+//     const clients = await Clients.find({Email: email2});
+//     if (clients == undefined || clients ==  null || clients.length== 0)
+//      {
+//         console.log("Could not find usre");
+//         return res.send("Could not find user.");
+//      }
 
-    const user = clients[0];
-    user.reset_password_token = token;
-    await user.save();
-    return res.send("DONE!");
-    // save t odb to email given by user
-}
+//     const user = clients[0];
+//     user.reset_password_token = token;
+//     await user.save();
+//     return res.send("DONE!");
+//     // save t odb to email given by user
+// }
 
 
 
--- controllers/reset_pass_cont.js
+++ controllers/reset_pass_cont.js
@@ -1,24 +1,24 @@
-const Clients = require("../models/clientschema");
+// const Clients = require("../models/clientschema");
 
 
-exports.setnew = async (req, res) => {
-    console.log("----------------");
-    console.log("======================");
-    console.log("======================");
-    var usrtoken= req.query.token;
-    const clients = await Clients.find({reset_password_token: usrtoken});
-    if(clients == undefined || clients == null || clients.length == 0 )
-    {
-    console.log("no token found" );
-    return res.send("couldn't find token ");
-    }
-    return res.render("set_new_pass");
+// exports.setnew = async (req, res) => {
+//     console.log("----------------");
+//     console.log("======================");
+//     console.log("======================");
+//     var usrtoken= req.query.token;
+//     const clients = await Clients.find({reset_password_token: usrtoken});
+//     if(clients == undefined || clients == null || clients.length == 0 )
+//     {
+//     console.log("no token found" );
+//     return res.send("couldn't find token ");
+//     }
+//     return res.render("set_new_pass");
   
-}
+// }
 
-exports.save_new_password = async (req, res) => {
-    // search for user
-    // update password field
-    // save to DB
-}
+// exports.save_new_password = async (req, res) => {
+//     // search for user
+//     // update password field
+//     // save to DB
+// }
 
