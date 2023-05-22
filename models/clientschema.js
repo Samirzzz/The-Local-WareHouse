87bee83cf775c16e22ca5fcd9c7ad3450850157d
Moo <mohamedaly4122003@gmail.com>
2023-05-22 22:56:48 +0200

    mm

--- models/clientschema.js
+++ models/clientschema.js
@@ -66,16 +66,70 @@ module.exports=Clients;
 
 
 
-//clientschema inside productschema
-
-// const orderschema=new Schema({
-//   type:string,
-//   required:true,
-//   child:clientSchema,
-// });
+//orderschema inside productschema
 
 
-// const Order=mongoose.model('Order',orderschema);
-// module.exports=Order;
+const orderschema=new Schema({
+  child:clientSchema,
+  client:{
+    type:String,
+    required:true,
+  },
+  orderproduct:{
+    type:String,
+    required:true,
+  },
+  orderprice:{
+    type:Number,
+    required:true,
+    default:0,
+  },
+  orderdate:String,
+  status:{
+    type:Boolean,
+    default:false,
+  },
+},
+{timestamps:true});
 
+
+const Order=mongoose.model('Order',orderschema);
+module.exports=Order;
+
+exports.createorder=async(req,res)=>{
+  const neworder=new order(req.body)
+  neworder.save();
+
+  return res.status(200).json({
+    client:neworder.client,
+    orderproduct:neworder.orderproduct,
+    orderprice:neworder.orderprice,
+    orderdate:neworder.orderdate,
+    status:neworder.status
+  })
+}
+
+// const[order, setOrder] = useRef(); 
+
+const handleSubmit = async (e) => {
+      e.preventDefault();
+      try {
+        const res = await post(GET_ORDERS_URL, {
+          client: {
+            firstname: firstname,
+            lastname: lastname,
+            numTel: numTel,
+            address: address,
+          },
+          orderproduct: orderproduct,
+          orderprice: orderprice,
+          orderdate: orderdate,
+        });
+
+        setOrder(res.data);
+        console.log("Order ", order);
+      } catch (error) {
+        console.log(error);
+      }
+  };
 
