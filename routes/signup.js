c8b740d0aecce88ec0cd0e09aa2693347781580a
Abdelrahman <abdelrahman2108840@miuegypt.edu.eg>
2023-05-26 19:44:49 +0300

    Controllers

--- routes/signup.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-var router = Router();
- 
-/* GET SignUp page. */
-    router.get('/',(req,res)=>{  
-    res.render('signup', { user: (req.session.user === undefined ? "" : req.session.user) });
-    });
- 
- module.exports = router;
--- server.js
+++ server.js
@@ -25,14 +25,14 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 
 var index_router = require("./routes/index.js");
-var login_router = require("./routes/login.js");
-var signup_router = require("./routes/signup.js");
+// var login_router = require("./routes/login.js");
+var signup_router = require("./routes/user.js");
 var admin_router = require("./routes/admin.js");
 var product_router = require("./routes/product.js");
 var edit_router = require("./routes/Account.js");
 var forget_router = require("./routes/forget.js");
 var wishlist_router = require("./routes/wishlist.js");
-var reset_pass=require("./routes/reset_pass")
+var reset_pass=require("./routes/reset_pass");
 
 const port = 3000
 
@@ -47,33 +47,7 @@ app.get('/product-details', (req, res) => {
     res.render('product-details', { user: (req.session.user === undefined ? "" : req.session.user) });
 })
 
-app.post("/signup", (req, res) => {
 
-
-    var query = { "Email": req.body.Email };
-
-    clients.find(query)
-        .then(result => {
-            if (result.length > 0) {
-                res.send('email taken');
-
-            }
-            else {
-                  const emp = new clients({
-                        username: req.body.username,
-                        Email: req.body.Email,
-                        password: req.body.password,
-                        Type: req.body.type,
-                        phonee: req.body.phonee,
-                        birth: req.body.date,
-                        gender: req.body.gender
-                })
-                emp.save();
-                console.log(req.body.password);
-                res.redirect('/');
-            }
-        });
-});
 
 app.use(fileUpload());
 app.post("/admin/addproduct", (req, res) => {
@@ -87,10 +61,8 @@ app.post("/admin/addproduct", (req, res) => {
     uploadPath = __dirname + '/public/images/' + req.files.img.name;
 
     imgfile.mv(uploadPath, function (err) {
-
         if (err)
         return res.status(500).send(err);
-
         const prod = new product({
             id: req.body.id,
             name: req.body.name,
@@ -118,49 +90,13 @@ app.get('/product', (req, res) => {
 });
 
 
-app.get('/logout', (req, res) => {
-    req.session.destroy();
-    res.redirect('/');
-});
+// app.get('/logout', (req, res) => {
+//     req.session.destroy();
+//     res.redirect('/');
+// });
 
 
-app.post('/login',[check('Email').trim().isEmail().withMessage('enter valid email'),
-check('password').trim().isLength(4).withMessage('min password length 4')] ,async function  (req, res) {
-    const user = { "Email": req.body.Email };
-    const errors=validationResult(req);
-    if(!errors.isEmpty()){
-        res.send('error');
-        errors.array(); 
-    }
-    clients.findOne(user).then(async result=>{
-        
-        
-        if(result==null){
-            res.send('email does not exist');
 
-        }
-        console.log(req.body.Email);
-        console.log(req.body.password);
-        console.log(result.Email);
-        console.log(result.password);
-        req.session.user=result;
-        const valid= await crypt.compare(req.body.password,result.password);
-           if(valid==true){
-   
-               res.redirect('/');
-           }
-           else{
-               res.send('false');
-          
-       } 
-    
-        
-       
-    })
-        .catch(err => {
-            console.log(err);
-        });
-});
 app.get('/profile', (req, res) => {
   
     res.render('profile', { user: (req.session.user === undefined ? "" : req.session.user) });
@@ -186,8 +122,8 @@ app.get('/profile', (req, res) => {
 
 //setup routes
 app.use('/', index_router);
-app.use('/login', login_router);
-app.use('/signup', signup_router);
+// app.use('/login', login_router);
+app.use('/user', signup_router);
 app.use('/admin', admin_router);
 app.use('/product', product_router);
 app.use('/forget', forget_router);
