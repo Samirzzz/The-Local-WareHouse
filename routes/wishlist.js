bd401ade23abdb7a2a40e901ba933d5dc52a5025
Abdelrahman <abdelrahman2108840@miuegypt.edu.eg>
2023-05-30 23:31:46 +0300

    wishlist routing

--- routes/wishlist.js
+++ routes/wishlist.js
@@ -1,10 +1,17 @@
 const { Router } = require('express');
+var bodyParser = require('body-parser');
 
 var router = Router();
+const user = require("../controllers/usercontrol");
+router.use(bodyParser.json());
+///wishlist
 
 /* GET wishlist page. */
 router.get('/', function(req, res, next) {
         res.render('wishlist', { user: (req.session.user === undefined ? "" : req.session.user) });
     });
+    router.post('/:productId', user.addToWishlist);
+    //  router.post('/:productId', user.addToWishlist);
+
 
 module.exports = router;
