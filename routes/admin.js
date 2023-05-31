499fc458f103c240152de1916ed961ad1a0fe59b
YoussefBakry5 <125702757+YoussefBakry5@users.noreply.github.com>
2023-05-31 22:50:18 +0300

    admin edit

--- routes/admin.js
+++ routes/admin.js
@@ -7,6 +7,7 @@ const router = express.Router();
 router.use(bodyParser.json());
 
 const Admin1 = require("../controllers/Adminproductcontrol");
+const Admin2 = require("../controllers/Adminusercontrol");
 /* GET Admin page. */
 
 router.use((req, res, next) => {
@@ -31,26 +32,10 @@ router.get('/adduser',(req,res)=>{
     res.render('adduser', { user: (req.session.user === undefined ? "" : req.session.user) });
     
 })
-
-/* GET Admin/searchedituser page. */
-router.get('/searchedituser',(req,res)=>{
-    res.render('searchedituser', { user: (req.session.user === undefined ? "" : req.session.user) });
-}) 
-
-/* GET Admin/edituser page. */
-router.get('/edituser',(req,res)=>{
-    res.render('edituser', { user: (req.session.user === undefined ? "" : req.session.user) });
-})
-
-/* GET Admin/searchbanuser page. */
-router.get('/searchbanuser',(req,res)=>{
-    res.render('searchbanuser', { user: (req.session.user === undefined ? "" : req.session.user) });
-})
-
-/* GET Admin/banuser page. */
-router.get('/banuser',(req,res)=>{
-    res.render('banuser', { user: (req.session.user === undefined ? "" : req.session.user) });
-})
+router.get('/view&edituser', Admin2.GetAllUsers);
+router.get("/toAdmin/:id", Admin2.toAdmin);
+router.get("/toClient/:id", Admin2.toClient);
+router.get("/delete/:id", Admin2.DeleteUser);
 
 /* GET Admin/addproduct page. */
 
@@ -81,14 +66,6 @@ router.get('/removeprod',(req,res)=>{
     res.render('removeprod', { user: (req.session.user === undefined ? "" : req.session.user) });
 })
 
-router.get('/view&edituser',(req,res)=>{
-    clients.find()
-    .then(result => {
-      res.render('view&edituser', { employees: result, user: (req.session.user === undefined ? "" : req.session.user) });
-    })
-    .catch(err => {
-      console.log(err);
-    });
-})
+
 
 module.exports = router;
