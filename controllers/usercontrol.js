3a7fb03896f039e714c5a626c858fb7598c8f84e
Samirzzz <abdelrahman.200300@gmail.com>
2023-06-03 02:14:38 +0300

    backendval

--- controllers/usercontrol.js
+++ controllers/usercontrol.js
@@ -5,44 +5,7 @@ const crypt = require("bcrypt");
 const path = require('path');
 const {check,validationResult}=require('express-validator');
 
-const AddUser =[ check('username')
-.notEmpty()
-.withMessage('Username is required')
-.isLength({ max: 50 })
-.withMessage('Username cannot be longer than 50 characters'),
-
-check('Email')
-.notEmpty()
-.withMessage('Email is required')
-.isEmail()
-.withMessage('Invalid email address'),
-
-check('password')
-.notEmpty()
-.withMessage('Password is required')
-.isLength({ min: 4 })
-.withMessage('Password must be at least 6 characters long'),
-
-check('phonee')
-.notEmpty()
-.withMessage('Phone number required')
-.optional({ nullable: true })
-.isMobilePhone()
-.withMessage('Invalid phone number'),
-
-check('address')
-.notEmpty()
-.withMessage('address required')
-.optional({ nullable: true })
-.isLength({ max: 100 })
-.withMessage('Address cannot be longer than 100 characters'),
-
-check('gender')
-.notEmpty()
-.withMessage('gender required')
-.optional({ nullable: true })
-.isIn(['male', 'female', 'other'])
-.withMessage('Gender must be either "male", "female", or "other"') , (req, res) => {
+const AddUser = (req, res) => {
    
   
     
@@ -57,8 +20,10 @@ check('gender')
               console.log(errors)
               res.send('Email taken');
             }else if(!errors.isEmpty()){
-            
-            return res.send({errors:errors.array()})
+           const alert=errors.array();
+          res.render('AddUser',{
+           alert
+          })
         }
         
         else {
@@ -79,7 +44,7 @@ check('gender')
         }
       });
     
-  }]
+  }
 
 
 
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 const express = require("express");
 var bodyParser = require('body-parser');
-const { check, validationResult } = require('express-validator');
 const router = express.Router();
 router.use(bodyParser.json());
+const clients = require('../models/clientschema');
+const {check,validationResult}=require('express-validator');
 
 const User = require("../controllers/usercontrol");
 /* GET SignUp page.
@@ -17,8 +18,72 @@ const User = require("../controllers/usercontrol");
     });
 
     router.post('/login', User.logs);
-    router.post('/signup', User.AddUser);
-    router.post('/AddUser', User.AddUser);
+    router.post('/signup',[check('username').isLength({min:4}).withMessage('min 4 characters'),
+    //     check('username,Email,password,phonee,address,gender')
+    // .notEmpty()
+    // .withMessage('field is required'),
+   
+    
+    check('Email')
+    .isEmail()
+    .withMessage('Invalid email address'),
+    
+    check('password')
+    .isLength({ min: 4 })
+    .withMessage('Password must be at least 4 characters long'),
+    
+    check('phonee')
+  
+    .isMobilePhone()
+    .withMessage('Invalid phone number'),
+    
+    check('address')
+    
+    .isLength({ max: 100 })
+    .withMessage('Address cannot be longer than 100 characters'),
+    
+    check('gender')
+    .optional({ nullable: true })
+    .isIn(['male', 'female', 'other'])
+    .withMessage('choose gender"')],(req,res)=>{
+        const errors = validationResult(req);
+        var query = { "Email": req.body.Email };
+        clients.find(query)
+          .then(result => {
+              if (result.length > 0 ) {
+                  
+                  console.log(errors)
+                  res.send('Email taken');
+                }else if(!errors.isEmpty()){
+               const alert=errors.array();
+              res.render('signup',{
+               alert
+              })
+            }
+            
+            else {
+              const emp = new clients({
+                username: req.body.username,
+                Email: req.body.Email,
+                password: req.body.password,
+                Type: req.body.type,
+                phonee: req.body.phonee,
+                address: req.body.address,
+                gender: req.body.gender
+              })
+              emp.save();
+              req.session.user=result;
+      
+              console.log(req.body.password);
+              res.redirect('/');
+            }
+          });
+
+
+
+
+    } );
+
 
 
     router.get('/logout', (req, res) => {
